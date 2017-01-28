083b879e1c157d6527388a9119db3701dc80fdff
Henry Tran <htran48@uwo.ca>
2017-01-28 18:32:11 -0500

    add call number

--- DeltaHacksAndroid/app/src/main/java/com/example/henrytran/deltahacksandroid/MainActivity.java
+++ DeltaHacksAndroid/app/src/main/java/com/example/henrytran/deltahacksandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.henrytran.deltahacksandroid;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     Location mLastLocation;
 
+    String LOG_TAG = this.getClass().getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
+            Log.d(LOG_TAG, mGoogleApiClient.toString());
         }
     }
 
@@ -75,5 +80,21 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    private void callNumber() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:123456789"));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 }
