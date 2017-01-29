c1ed012283cbfb755e32cfabe0be5804c5f95c49
Henry Tran <htran48@uwo.ca>
2017-01-29 01:37:40 -0500

    connects to server

--- DeltaHacksAndroid/app/src/main/java/com/example/henrytran/deltahacksandroid/MainActivity.java
+++ DeltaHacksAndroid/app/src/main/java/com/example/henrytran/deltahacksandroid/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.henrytran.deltahacksandroid;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
@@ -16,10 +15,9 @@ import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.gson.Gson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -49,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 case DATA_MESSAGE:
                     double[] posVals = (double[]) msg.obj;
 
-                    Log.d(LOG_TAG, "x = " + posVals[0] + "\ny = " + posVals[1] + "\nz = " + posVals[2]);
+                    Log.e(LOG_TAG, "x = " + posVals[0] + "\ny = " + posVals[1] + "\nz = " + posVals[2]);
 
                     // TODO Update UI
             }
@@ -61,43 +59,45 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive);
 
-        // Create an instance of GoogleAPIClient
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-            Log.d(LOG_TAG, mGoogleApiClient.toString());
-        }
+//        // Create an instance of GoogleAPIClient
+//        if (mGoogleApiClient == null) {
+//            mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                    .addConnectionCallbacks(this)
+//                    .addOnConnectionFailedListener(this)
+//                    .addApi(LocationServices.API)
+//                    .build();
+//        }
+//
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        Log.e(LOG_TAG, String.valueOf(LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient).getLatitude()));
+//        // Get eContactNumber from shared preferences
+//        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+//        Gson gson = new Gson();
+//        String json = prefs.getString("EContact", "");
+//        mEContact = gson.fromJson(json, Econtact.class);
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Log.d(LOG_TAG, String.valueOf(LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient).getLatitude()));
-        // Get eContactNumber from shared preferences
-        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = prefs.getString("EContact", "");
-        mEContact = gson.fromJson(json, Econtact.class);
+        FetchDataThread fetchDataThread = new FetchDataThread();
+        fetchDataThread.start();
     }
 
     @Override
     protected void onStart() {
-        mGoogleApiClient.connect();
+//        mGoogleApiClient.connect();
         super.onStart();
     }
 
     @Override
     protected void onStop() {
-        mGoogleApiClient.disconnect();
+  //      mGoogleApiClient.disconnect();
         super.onStop();
     }
 
@@ -117,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 mGoogleApiClient);
         if (mLastLocation != null) {
 
-            Log.d("MainActivity", String.valueOf(mLastLocation.getLatitude()) + ", " + String.valueOf(mLastLocation.getLongitude()));
+            Log.e("MainActivity", String.valueOf(mLastLocation.getLatitude()) + ", " + String.valueOf(mLastLocation.getLongitude()));
         }
     }
 
@@ -153,17 +153,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      *
      */
     private class FetchDataThread extends Thread {
+
         public void run() {
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             String jsonData = null;
 
-
             while (true) {
                 try {
-                    // TODO Add url
                     // Create request to http server
-                    URL url = new URL("https://httpbin.org/get");
+                    URL url = new URL("http://172.17.43.101:8080");
                     urlConnection = (HttpURLConnection) url.openConnection();
                     urlConnection.setRequestMethod("GET");
                     urlConnection.connect();
@@ -175,8 +174,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     reader = new BufferedReader(new InputStreamReader(inputStream));
 
                     String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line + "\n");
+                        Log.e(LOG_TAG, "Line: " + line);
+                    }
 
                     jsonData = buffer.toString();
+                    Log.e(LOG_TAG, jsonData);
                     parseJsonData(jsonData);
                 } catch (IOException e) {
                     Log.e(LOG_TAG, "Error ", e);
@@ -207,13 +211,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
          * @throws JSONException
          */
         private void parseJsonData(String jsonStr) throws JSONException {
-            JSONObject dataJson = new JSONObject(jsonStr);
-
+            JSONArray dataJsonArray = new JSONArray(jsonStr);
             // x, y, z values
             double[] posVals = new double[3];
-            posVals[0] = dataJson.getDouble("x");
-            posVals[1] = dataJson.getDouble("y");
-            posVals[2] = dataJson.getDouble("z");
+            posVals[0] = dataJsonArray.getDouble(0);
+            posVals[1] = dataJsonArray.getDouble(1);
+            posVals[2] = dataJsonArray.getDouble(2);
+
+            Log.e(LOG_TAG, "x = " + String.valueOf(posVals[0]) + "\ny = " + String.valueOf(posVals[1]) + "\nz = " + String.valueOf(posVals[2]));
 
             // Send data to handler
             mHandler.obtainMessage(DATA_MESSAGE, posVals);
