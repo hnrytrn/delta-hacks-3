249cdc7535c293a9b5aad67565ec0b49b0f0afcf
Liam Duncan <ldunca7@uwo.ca>
2017-01-29 07:12:07 -0500

    logo

--- DeltaHacksAndroid/app/src/main/java/com/example/henrytran/deltahacksandroid/welcomeActivity.java
+++ DeltaHacksAndroid/app/src/main/java/com/example/henrytran/deltahacksandroid/welcomeActivity.java
@@ -3,9 +3,11 @@ package com.example.henrytran.deltahacksandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class welcomeActivity extends AppCompatActivity {
 
@@ -16,7 +18,7 @@ public class welcomeActivity extends AppCompatActivity {
 
 
 //        Set Button Listener
-        ((Button) findViewById(R.id.enterButton)).setOnClickListener(new View.OnClickListener() {
+        ((ImageView) findViewById(R.id.wheel)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("LD", "Enter button of welcome pressed");
